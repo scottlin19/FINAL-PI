c63be2ec9f9b27b713eac524164682a3486edf5f
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-10 19:42:21 -0300

    Update listaAeropuerto.c

--- listaAeropuerto.c
+++ listaAeropuerto.c
@@ -115,7 +115,7 @@ cargarDatos(listaAeropuertoADT lista,char * pathA,char * pathM)
 		if(c == ';'){
 			cont++;
 			printf("cont  vale = %d \n",cont);
-			if(cont > CANT_CAMPOS_MOV){ //Si llego al final de los campos tengo que agregar el movimiento;
+			if(c == '\n'){ //Si llego al final de los campos tengo que agregar el movimiento;
 				cont = 0;	
 				printf("nombre = %s\norigen = %s\ndestino = %s\nclase = %d\nclasificacion = %d\ntipo = %d\ndia = %d\n",datos->nombre,datos->origen,datos->destino,datos->clase_vuelo,datos->clasificacion_vuelo,datos->tipo_vuelo,datos->dia);		
 			}
