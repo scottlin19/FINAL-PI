eefc01c2d7d6e7761060be9878ed0cd66a52ccfa
scottlin19 <scott.lin099@gmail.com>
2018-12-10 18:52:56 -0300

    punteros

--- listaAeropuerto.c
+++ listaAeropuerto.c
@@ -106,7 +106,7 @@ cargarDatos(listaAeropuertoADT lista,char * pathA,char * pathM)
 	char c;
 	char s[MAX_TEXTO];
 	int index;
-	 tDatos * datos = malloc(sizeof(tDatos)); 
+	tDatos * datos = malloc(sizeof(tDatos)); 
 	while((c = fgetc(archM)) != '\n');
 	printf("c vale %c \n",c);
 	while((c = fgetc(archM)) != EOF){
@@ -115,7 +115,7 @@ cargarDatos(listaAeropuertoADT lista,char * pathA,char * pathM)
 			cont++;
 			if(cont > CANT_CAMPOS_MOV){ //Si llego al final de los campos tengo que agregar el movimiento;
 				cont = 0;	
-				printf("nombre = %s\norigen = %s\ndestino = %s\nclase = %d\nclasificacion = %d\ntipo = %d\ndia = %d\n",datos.nombre,datos.origen,datos.destino,datos.clase_vuelo,datos.clasificacion_vuelo,datos.tipo_vuelo,datos.dia);		
+				printf("nombre = %s\norigen = %s\ndestino = %s\nclase = %d\nclasificacion = %d\ntipo = %d\ndia = %d\n",datos->nombre,datos->origen,datos->destino,datos->clase_vuelo,datos->clasificacion_vuelo,datos->tipo_vuelo,datos->dia);		
 			}
 		}
 		if(cont == 0){
