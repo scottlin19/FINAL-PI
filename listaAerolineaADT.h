9be35c7d630383854b02076128f8bc84708760d2
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-11 20:27:45 -0300

    Update listaAerolineaADT.h

--- listaAerolineaADT.h
+++ listaAerolineaADT.h
@@ -1,2 +1,8 @@
+
+#include "listaAeropuertoADT.h"
+
 typedef struct listaAerolineaCDT* listaAerolineaADT;
 
+listaAreolineaADT nuevaListaAL(void);
+
+int cargarDatosAL(listaAerolineaADT lista,char * pathM);
