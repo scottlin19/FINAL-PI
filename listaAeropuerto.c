3ce3f5f81f94fd81a17a4ed5a69e55ce662ac311
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-11 15:58:39 -0300

    Update listaAeropuerto.c

--- listaAeropuerto.c
+++ listaAeropuerto.c
@@ -4,12 +4,14 @@
  *
  */
 /* Version recursiva de listas implementadas dinamicamente */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "listaAeropuerto.h"
 #include <string.h>
-#define MAX_DENOM 70
+/*
+
 #define DIAS_SEMANA 7
 #define FECHA 0
 #define CLASE 2
@@ -27,16 +29,21 @@
 #define NO_REGULAR 1
 #define PRIVADO 2
 #define CANT_CAMPOS_MOV 10
+
+*/
+#define OACI 1
+#define DENOMINACION 4
+#define PROVINCIA 21
+#define MAX_DENOM 70
 #define MAX_TEXTO 250
-
-typedef enum {DOMINGO = 0,LUNES ,MARTES,MIERCOLES,JUEVES,VIERNES,SABADO} dias_semana;
-
+/*
 typedef struct tMov{
 	int cant_cabotaje;
 	int cant_internacional;
 
 }tMov;
-
+*/
+/*
 typedef struct tAerolinea {
 	char * nombre;
 	tMov cant_mov[DIAS_SEMANA]; // Si estan en una aerolinea significa que son vuelos REGULARES.
@@ -48,7 +55,8 @@ typedef struct tAerolinea {
 }tAerolinea;
 
 typedef struct tAerolinea * tAerolineaP;
-
+*/
+/*
 typedef struct tDatos{
 	char * origen;
 	char * destino;
@@ -59,15 +67,19 @@ typedef struct tDatos{
 	int dia;
 }tDatos;
 
+*/
 
+typedef struct tDatos{
+	char * oaci; 
+	char * denom;
+	char * prov;
+}tDatos;
 
 typedef struct tAeropuerto {
 	char * OACI;
 	char denominacion[MAX_DENOM];
-	char * provincia;
-	
+	char * provincia;	
 	tMov cant_mov_no_regulares[DIAS_SEMANA][2]; //  0 = Vuelos no regulares , 1 = Vuelos privados;
-	tAerolineaP first_Aerolinea;
 	
 	int mov_totales;
 	struct tAeropuerto * siguiente;
@@ -78,18 +90,26 @@ typedef struct tAeropuerto {
 
 typedef struct tAeropuerto * tAeropuertoP;
 
+typedef struct listaAerolineaCDT {
+	tAeropuertoP primero;
+
+	tAeropuertoP next;
+}listaAeropuertoCDT;
+
+
 typedef struct listaAeropuertoCDT {
 	tAeropuertoP primero;
 
 	tAeropuertoP next;
 }listaAeropuertoCDT;
 
+
 static comparaMov(tAeropuertoP a1,tAeropuertoP a2)
 {
 	return a1->mov_totales - a2->mov_totales;
 
 }
-
+/*
 static tAeropuertoP insertRec(tAeropuertoP primero, tDatos datos,FILE * archA , int * added) {
 	
 	if( primero == NULL || (primero->mov_totales == 1) && strcmp(primero->OACI,datos->origen) > 0)
@@ -128,12 +148,62 @@ diaDeLaSemana(int d, int m, int a)
 	return   (d+=m<3?a--:a-2,23*m/9+d+4+a/4-a/100+a/400)%7  ; //Retorna el dia de la semana 0 es domingo, 1 es lunes, etc;
 
 }
-
+*/
 void
-cargarDatos(listaAeropuertoADT lista,char * pathA,char * pathM)
+cargarDatos(listaAeropuertoADT lista, char * pathA)
+{
+
+	FILE * archA = fopen(pathA,"rt"); //Abro archivo aeropuerto.csv
+	
+	
+	if(archA == NULL|| archM == NULL){
+		printf("Error al abrir los archivos. \n");
+	
+	}
+	
+	
+	int cont = 0;	
+	int valido = 1;
+	int index;
+	char * token;
+	tDatos * datos = malloc(sizeof(tDatos)); 
+	char  s[MAX_TEXTO];
+	fgets(s,MAX_TEXTO,archM);
+	while(fgets(s,MAX_TEXTO,archA) != NULL){
+		printf("s = %s\n",s);
+		token = strtok(s,";");
+		cont = 0;
+		while(token != NULL & valido){
+			if(cont == OACI){
+				if(!isspace(token)){
+					datos->oaci = token;
+					
+				}else{
+					valido = 0;
+				}
+			}else if(valido && cont == DENOMINACION){	
+					datos->denom = token;
+			}else if(valido && cont == PROVINCIA){	
+					datos->prov = token;
+					
+			}
+		
+			
+			cont++;
+			token =  strtok(NULL, ";");
+		}	
+		valido = 1;
+		printf("OACI: %s\nDENOM: %s\nPROVINCIA: %s\n",datos->oaci,datos->denom,datos->prov);
+
+	}
+}
+
+/*
+void
+cargarDatos(listaAeropuertoADT lista,char * pathA)
 {
 	FILE * archA = fopen(pathA,"rt"); //Abro archivo aeropuerto.csv
-	FILE * archM = fopen(pathM,"rt");//Abro archivo movimientos.csv
+	
 	
 	if(archA == NULL|| archM == NULL){
 		printf("Error al abrir los archivos. \n");
@@ -255,7 +325,7 @@ cargarDatos(listaAeropuertoADT lista,char * pathA,char * pathM)
 	}
 
 }
-
+*/
 
 static void
 Error(const char* s)
@@ -322,23 +392,24 @@ listSize(const listADT list) {
 	return list->size;
 }
 
+void
+toBegin(listADT list) {
+	list->next = list->first;
+}
+
 int
 hasNext(const listADT list) {
 	return list->next != NULL;
 }
 */
-
-void
-toBegin(listaAeropuertoADT listaAeropuerto) {
-	listaAeropuerto->next = listaAeropuerto->primero;
-}
-
-void
+tAeropuertoP
 next(listaAeropuertoADT listaAeropuerto) {
 	if (listaAeropuerto->next==NULL){
 		Error("No hay mas elementos a recorrer");
 	}
-	list->next = list->next->siguiente;
+	list->next = list->next->tail;
+
+	return list->next;
 }
 
 
