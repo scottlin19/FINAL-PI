76e20336b48557c5e3a853f170d54a3a9f22dbb0
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-12 00:34:48 -0300

    Update listaAeropuertoCDT.c

--- listaAeropuertoCDT.c
+++ listaAeropuertoCDT.c
@@ -69,7 +69,10 @@ insertarAPRec(tAeropuertoP primero,tDatosAP datos, int * ok)
 			printf("Error:No se pudo utilizar malloc\n");
 		}else{
 		aux->cola = primero;
-		aux->datos = datos;
+		aux->datos = malloc(sizeof(tDatosAP));
+		aux->datos.oaci = datos.oaci;
+		aux->datos.denom = datos.denom;
+		aux->datos.prov = datos.prov;
 		printf("Nuevo OICI: %s\nNueva Denom: %s\nNueva Prov: %s\n",aux->datos.oaci,aux->datos.denom,aux->datos.prov);
 		*ok = 1;
 		}
