6e757d5e53d088a9e811d4fed327538a8dbad48c
scottlin19 <scott.lin099@gmail.com>
2018-12-12 00:13:22 -0300

    querys mejor

--- censo.c
+++ censo.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "listaAeropuertoCDT.c"
 #include "listaAerolineaADT.h"
 
 void query1(listaAeropuertoADT listaAeropuerto, int *ok);
+void query2(listaAeropuertoADT listaAeropuerto, int *ok);
 
 int main(int argc, char * argv[])
 {
 	int ok = 0;
-	listaAeropuertoADT lista = nuevaListaAP();
+	listaAeropuertoADT listaAP = nuevaListaAP();
 	listaAerolineaADT listaAL = nuevaListaAL();
-	listaAL->primero->cant_mov_cabotages = 1;
-	if (cargarDatosAP(lista, "../aer.csv")){
+	if (cargarDatosAP(listaAP, "../aer.csv")){
 		printf("funca\n");
 	}
-	query1(lista, &ok);
+	if (cargarDatosAL(listaAL, listaAP, "../mov.csv")){
+		printf("funca\n");
+	}
+	query1(listaAP, &ok);
+	printf("ok vale %d\n", ok);
+	query2(listaAP, &ok);
+	printf("ok vale %d\n", ok);
 	return ok;
 	
 
@@ -33,7 +39,10 @@ void query1(listaAeropuertoADT listaAeropuerto, int *ok){
 	else {
 		fprintf(archivoDest, "OACI;Denominación;Movimientos\n");
 		while(listaAeropuerto->proximo != NULL){
-			fprintf(archivoDest, "%s;%s;%d\n", listaAeropuerto->next->datos.OACI, listaAeropuerto->next->datos.denominacion, listaAeropuerto->next->mov_totales);
+			printf("oaci vale %s\n", listaAeropuerto->proximo->datos.oaci);
+			if (listaAeropuerto->proximo->mov_totales != 0){
+				fprintf(archivoDest, "%s;%s;%d\n", listaAeropuerto->proximo->datos.oaci, listaAeropuerto->proximo->datos.denom, listaAeropuerto->proximo->mov_totales);
+			}
 			proximoAP(listaAeropuerto);
 		}
 	}
@@ -49,20 +58,25 @@ void query2(listaAeropuertoADT listaAeropuerto, int *ok){
 	}
 	else {
 		fprintf(archivoDest, "Día;Cabotaje;Internacional;Total\n");
-		char dias[7][10] = {"lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"};
-		int cantCab, cantInter, dia = 0, i;
+		char dias[7][11] = {"lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"};
+		int total[7][2];
+		int cantCab, cantInter, dia = 0, clase;
 		alPrincipioAP(listaAeropuerto);
 		while (listaAeropuerto->proximo != NULL){
-			cantCab = cantInter = 0;
-			for (i = 0; i < 3;i++){
-				cantCab += listaAeropuerto->proximo->cant_mov[dia][i].cant_cabotaje;
-				cantInter += listaAeropuerto->proximo->cant_mov[dia][i].cant_internacional;
+			for (dia = 0; dia < 7; dia++){	
+				cantCab = cantInter = 0;
+				for (clase = 0; clase < 3;clase++){
+					total[dia][0] += listaAeropuerto->proximo->cant_mov[dia][clase].cant_cabotaje;
+					total[dia][1] += listaAeropuerto->proximo->cant_mov[dia][clase].cant_internacional;
+				}
 			}
-			fprintf(archivoDest, "%s;%d;%d;%d", dias[dia], cantCab,cantInter,cantCab + cantInter);
-			dia++;
+			listaAeropuerto->proximo = listaAeropuerto->proximo->cola;
 		}
-		fclose(archivoDest);
+		for (dia = 0; dia < 7; dia++){
+			fprintf(archivoDest, "%s;%d;%d;%d\n", dias[dia], total[dia][0],total[dia][1],total[dia][0] + total[dia][1]);
+		}
 	}
+	fclose(archivoDest);
 }
 /*
 void query3(listaAeropuertoADT listaAeropuerto, int *ok){
