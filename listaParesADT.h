2e4d872a823093aac6ea875d83dcea9f7410f67d
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-12 22:00:27 -0300

    Update listaParesADT.h

--- listaParesADT.h
+++ listaParesADT.h
@@ -1,3 +1,8 @@
+#ifndef LISTAPARESADT_H_
+#define LISTAPARESADT_H_
+
 typedef struct listaParesCDT * listaParesADT;
 
 int insertarPares(listaParesADT lista,char * origen, char * destino);
+
+#endif
