81aa8052c98d3e7f6ea8d5c1604291278c1812f5
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-13 17:32:33 -0300

    Update listaAeropuertoCDT.c

--- listaAeropuertoCDT.c
+++ listaAeropuertoCDT.c
@@ -174,7 +174,7 @@ agregarMovAPrec(tAeropuertoP primero,char * oaci,char * clase, char * clasif, in
 			
 			int comp;
 			//printf("%s movs: %d,  %s movs: %d \n",primero->datos->oaci,primero->mov_totales,primero->cola->datos->oaci,primero->cola->mov_totales);
-			if((primero->cola != NULL )&& ((comp =(primero->mov_totales - primero->cola->mov_totales)) < 0) || ((comp == 0) &&  (strcmp(primero->datos.oaci,primero->cola->datos.oaci) > 0) )){
+			if( ((primero->cola != NULL ) && ((comp =(primero->mov_totales - primero->cola->mov_totales)) < 0)) || ((comp == 0) &&  (strcmp(primero->datos.oaci,primero->cola->datos.oaci) > 0) )){
 						
 						tAeropuertoP aux = primero->cola->cola;
 						primero->cola->cola = primero;
