80b61d002f9a86ed4ca80395677515d627d92f5d
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-13 17:31:36 -0300

    Update listaParesADT.h

--- listaParesADT.h
+++ listaParesADT.h
@@ -6,6 +6,6 @@ typedef struct listaParesCDT * listaParesADT;
 int insertarPares(listaParesADT lista,char * provincias[]);
 
 listaParesADT nuevaListaPares(void);
-
+void printListaPares(listaParesADT lista);
 void query4(listaParesADT lista, int *ok);
 #endif
