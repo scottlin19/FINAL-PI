98c8f46f40bef4f05b77202224b6de029cbdf76b
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-13 20:10:50 -0300

    Update censo.c

--- censo.c
+++ censo.c
@@ -22,8 +22,8 @@ int main(int argc, char * argv[])
 	printf("ok vale %d\n", ok);
 	query3(listaAP, &ok);
 	printf("ok vale %d\n", ok);
-	//query4(listaPares, &ok);	
-	//printf("ok vale %d\n", ok);
+	query4(listaPares, &ok);	
+	printf("ok vale %d\n", ok);
 	query5(listaAL,&ok);
 	printf("ok vale %d\n", ok);
 	return ok;
