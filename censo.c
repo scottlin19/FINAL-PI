32c25ba7a6f3845803052740ce4256b732e971e9
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-13 20:10:36 -0300

    Update censo.c

--- censo.c
+++ censo.c
@@ -8,14 +8,14 @@ int main(int argc, char * argv[])
 {
 	int ok = 0;
 	listaAeropuertoADT listaAP = nuevaListaAP();
-	//listaAerolineaADT listaAL = nuevaListaAL();
+	listaAerolineaADT listaAL = nuevaListaAL();
 	listaParesADT listaPares = nuevaListaPares();
 	if (cargarDatosAP(listaAP, argv[1])){
 		printf("funca\n");
 	}
-	//if (cargarDatosAL(listaAL, listaAP,listaPares, argv[2])){
-	//	printf("funca\n");
-	//}
+	if (cargarDatosAL(listaAL, listaAP,listaPares, argv[2])){
+		printf("funca\n");
+	}
 	query1(listaAP, &ok);
 	printf("ok vale %d\n", ok);
 	query2(listaAP, &ok);
@@ -24,8 +24,8 @@ int main(int argc, char * argv[])
 	printf("ok vale %d\n", ok);
 	//query4(listaPares, &ok);	
 	//printf("ok vale %d\n", ok);
-	//query5(listaAL,&ok);
-	//printf("ok vale %d\n", ok);
+	query5(listaAL,&ok);
+	printf("ok vale %d\n", ok);
 	return ok;
 	
 
