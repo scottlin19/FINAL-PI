3292870bcf66f143217923e45f7e96ae5334f316
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-14 15:04:08 -0300

    Update censo.c

--- censo.c
+++ censo.c
@@ -5,14 +5,23 @@
 #include "listaAerolineaADT.h"
 
 int cargarMovimientos(listaAerolineaADT listaAL,listaAeropuertoADT listaAP,listaParesADT listaPares,char * pathM);
-
+int cargarAeropuertos(listaAeropuertoADT lista, char * pathA);
 
 #define MAX_TEXTO 400
-#define MAX_OACI 15
+
 #define MAX_NOMBRE 30
 #define MAX_CLASE 40
 #define MAX_TIPO 10
 #define MAX_CLASIF 40
+
+#define MAX_OACI 15
+#define MAX_DENOM 70
+#define MAX_PROV  70
+
+#define OACI 1
+#define DENOMINACION 4
+#define PROVINCIA 21
+#define DIAS_SEMANA 7
 
 #define FECHA 0
 #define CLASE 2
@@ -21,6 +30,12 @@ int cargarMovimientos(listaAerolineaADT listaAL,listaAeropuertoADT listaAP,lista
 #define ORIGEN 5
 #define DESTINO  6
 #define NOMBRE 7
+
+typedef struct tDatosAP{
+	char  oaci[MAX_OACI]; 
+	char  denom[MAX_DENOM];
+	char  prov[MAX_PROV];
+}tDatosAP;
 
 typedef struct tDatosMov{
 	char  origen[MAX_OACI];
@@ -33,28 +48,30 @@ typedef struct tDatosMov{
 	int dia;
 }tDatosMov;
 
+
+
 int main(int argc, char * argv[])
 {
 	int ok = 0;
 	listaAeropuertoADT listaAP = nuevaListaAP();
 	listaAerolineaADT listaAL = nuevaListaAL();
 	listaParesADT listaPares = nuevaListaPares();
-	if (cargarDatosAP(listaAP, argv[1])){
+	if (cargarAeropuertos(listaAP, argv[1])){
 		printf("funca\n");
 	}
-	if (cargarDatosAL(listaAL, listaAP,listaPares, argv[2])){
+	if (cargarMovimientos(listaAL, listaAP,listaPares, argv[2])){
 		printf("funca\n");
 	}
 	query1(listaAP, &ok);
-	printf("ok vale %d\n", ok);
+
 	query2(listaAP, &ok);
-	printf("ok vale %d\n", ok);
+	
 	query3(listaAP, &ok);
-	printf("ok vale %d\n", ok);
+
 	query4(listaPares, &ok);	
-	printf("ok vale %d\n", ok);
+	
 	query5(listaAL,&ok);
-	printf("ok vale %d\n", ok);
+	
 	return ok;
 	
 
@@ -68,6 +85,69 @@ diaDeLaSemana(int d, int m, int a)
 	return   (d+=m<3?a--:a-2,23*m/9+d+4+a/4-a/100+a/400)%7  ; //Retorna el dia de la semana 0 es domingo, 1 es lunes, etc;
 }
 
+
+int
+cargarAeropuertos(listaAeropuertoADT lista, char * pathA)
+{
+
+	FILE * archA = fopen(pathA,"rt"); //Abro archivo aeropuerto.csv
+		
+	if(archA == NULL){
+		printf("Error al abrir los archivos. \n");
+		return 1;
+	
+	}
+		
+	int cont;	
+	int valido;
+	char * token;
+	char  s[MAX_TEXTO];
+	tDatosAP  datos;
+	
+	fgets(s,MAX_TEXTO,archA); //Me salto la primera linea del archivo que contiene los nombres  de los campos.
+	
+	while(fgets(s,MAX_TEXTO,archA )!= NULL){
+		
+			token = strtok(s,";");
+			cont = 0;
+			valido =1;
+			
+			while((token != NULL) && valido){
+				if(cont == OACI){
+					if(strcmp(token," ") == 0){
+						valido = 0;
+
+					}else{		
+
+						strcpy(datos.oaci,token);
+					}
+
+				}else if(valido && cont == DENOMINACION){	
+						strcpy(datos.denom,token);
+
+				}else if(valido && cont == PROVINCIA){	
+						strcpy(datos.prov,token);
+
+				}	
+				cont++;
+
+				token =  strtok(NULL, ";");	
+
+			}	
+			
+			if(valido){ //Si es valido el aeropuerto tiene OACI
+					if(!insertarAP(lista,datos.oaci,datos.denom,datos.prov)){
+						printf("Error al cargar datos \n");
+						return 1;
+					}
+
+			}
+	
+	
+	}
+
+	return 0;
+}
 
 int
 cargarMovimientos(listaAerolineaADT listaAL,listaAeropuertoADT listaAP,listaParesADT listaPares,char * pathM)
