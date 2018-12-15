32e4f1dd868dffc9b3d810c4588e426497477a59
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-15 17:39:57 -0300

    Update listaAeropuertoADT.h

--- listaAeropuertoADT.h
+++ listaAeropuertoADT.h
@@ -9,11 +9,10 @@
 #define LISTAPADT_H_
 
 
-#define OACI 1
-#define DENOMINACION 4
-#define PROVINCIA 21
-
-#define MAX_OACI 5
+#define OACI 1   //Número del campo donde se encuentra el OACI en el aeropuertos.csv
+#define DENOMINACION 4 //Número del campo donde se encuentra la denominación en el aeropuertos.csv
+#define PROVINCIA 21 //Número del campo donde se encuentra la provincia en el aeropuertos.csv
+#define MAX_OACI 5 //Cantidad de letras de un OACI + el '\0'
 
 
 /* El siguiente tipo define el TAD Lista.
@@ -22,19 +21,8 @@
  */
 typedef struct listaAeropuertoCDT * listaAeropuertoADT;
 
-//Carga Datos del archivo aeropuertos.csv , devuelve 1 si hubo un error y 0 si no.
-int sonDistintasProv(listaAeropuertoADT lista,char * origen, char * destino, char * provincias[]);
-int agregarMovAP(listaAeropuertoADT lista,char * oaci,char * clase, char * clasif, int dia);
-void query1(listaAeropuertoADT listaAeropuerto, int *ok);
-void query2(listaAeropuertoADT listaAeropuerto, int *ok);
-void query3(listaAeropuertoADT listaAP, int *ok);
 
-/*
-static int compare(listElementT elem1, listElementT elem2 )
-{
-	return elem1 - elem2;		
-}
-*/
+
 /* Funcion:  		newList
  * Uso:  		list = newList();
  * -------------------------------------------------------------------
@@ -46,8 +34,9 @@ static int compare(listElementT elem1, listElementT elem2 )
 listaAeropuertoADT nuevaListaAP(void);
 
 
+
 /* Funcion:  		insertarAP
- * Uso:  	        res == 	insertAP(listAP, oaci, denom, prov);
+ * Uso:  	        res = insertAP(listAP, oaci, denom, prov);
  * -------------------------------------------------------------------
  * Descripción: 	Inserta un elemento en la lista ordenada,
  *  			usando la función de comparación Compare.
@@ -59,7 +48,56 @@ listaAeropuertoADT nuevaListaAP(void);
  */
 int insertarAP( listaAeropuertoADT listaAP, char * oaci, char * denom, char * prov);
 
+
+
+
+/* Funcion:  		agregarMovAP
+ * Uso:  	       	res = agregarMovAP(listAP, oaci, clase, clasif,dia);
+ * -------------------------------------------------------------------
+ * Descripción: 	Busca un aeropuerto con un OACI igual al que se
+ 			manda por parámetro. Si lo encuentra, le suma un 
+			movimiento segun la clase, clasificación y dia del movimiento.
+			Luego reordena si es necesario la lista de mayor a menor respecto
+			a cantidad de movimientos,y si las cantidades son iguales,
+			se los ordena alfabeticamente.
+			Si no lo encuentra envia un mensaje de error.	
+			Retorna ?
+ * -------------------------------------------------------------------
+ * Precondicion:	Lista que sea valida, previamente creada y cargada con los datos de aeropuertos.csv.
+ * Postcondicion: 	Lista con 1 movimiento más y reordenada, si encontró el aeropuerto.
+ */
+int agregarMovAP(listaAeropuertoADT lista,char * oaci,char * clase, char * clasif, int dia);
+
+
+
+/* Funcion:  		sonDistintasProv
+ * Uso:  	       	res = sonDistintasProv(listAP, origen, destino, provincias);
+ * -------------------------------------------------------------------
+ * Descripción: 	Busca en la lista de aeropuertos los OACI origen y destino y guarda sus provincias
+ 			en orden alfabético en el vector provincias.
+			Retorna 1 si son distintas provincias y 0 en caso contrario.
+ * -------------------------------------------------------------------
+ * Precondicion:	Lista de aeropuertos que sea valida, previamente creada, y cargada con los datos de aeropuertos.csv.
+ 			El vector provincias debe tener tamaño igual (o mayor) a 2.
+ * Postcondicion: 	- (lista invariante).
+ */
+int sonDistintasProv(listaAeropuertoADT lista,char * origen, char * destino, char * provincias[]);
+
+
+
+/* Funcion: 		freeAP
+ * Uso: 		freeAP( listaAP );
+ * -------------------------------------------------------------------
+ * Descripción: 	Destruye la lista, liberando recursos alocados.
+ * -------------------------------------------------------------------
+ * Precondicion:	Lista que sea valida.
+ * Postcondicion: 	-
+ */
 void freeAP(listaAeropuertoADT listaAP);
 
+
+void query1(listaAeropuertoADT listaAeropuerto, int *ok);
+void query2(listaAeropuertoADT listaAeropuerto, int *ok);
+void query3(listaAeropuertoADT listaAP, int *ok);
 
 #endif
