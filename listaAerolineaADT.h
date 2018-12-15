33081bc98fc5876b5aae825143773fe06e285e66
scottlin19 <scott.lin099@gmail.com>
2018-12-15 17:00:49 -0300

    hola

--- listaAerolineaADT.h
+++ listaAerolineaADT.h
@@ -2,9 +2,24 @@
 #define LISTAALADT_H_
 typedef struct listaAerolineaCDT* listaAerolineaADT;
 
+/* Funcion:                nuevaListaAL
+ * Uso:                    listaAL = nuevaListaAL();
+ * ---------------------------------------------
+ * Descripcion:            Inicializa la lista, no hay errores posibles.
+ * ---------------------------------------------
+ * Precondicion:           -
+ * Postcondicion:          Lista creada.
+ */
 listaAerolineaADT nuevaListaAL(void);
 
-
+/* Funcion:                freeAL
+ * Uso:                    freeAL(listaAL);
+ * ---------------------------------------------
+ * Descripcion:            Destruye la lista, liberandorecursos alocados.
+ * ---------------------------------------------
+ * Precondicion:           Lista que sea valida. 
+ * Postcondicion:          -
+ */
 void freeAL(listaAerolineaADT listaAL);
 
 int insertarAL( listaAerolineaADT lista, char * nombre);
