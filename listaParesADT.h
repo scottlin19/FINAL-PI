9d9924a4617de7e663497f3917e1898b5a612d56
scottlin19 <scott.lin099@gmail.com>
2018-12-15 15:47:19 -0300

    hola

--- listaParesADT.h
+++ listaParesADT.h
@@ -5,8 +5,38 @@ typedef struct listaParesCDT * listaParesADT;
 
 int insertarPares(listaParesADT lista,char * provincias[]);
 
+/* Funcion:                 nuevaListaPares
+ * Uso:                     listaPares = nuevaListaPares();
+ * ----------------------------------------------------
+ * Descripcion:             Inicializa la lista. No hay error posible.
+ * ----------------------------------------------------
+ *  Precondicion:           
+ *  Postcondicion:          Lista creada.
+ */
 listaParesADT nuevaListaPares(void);
+
 void printListaPares(listaParesADT lista);
+
+/* Funcion:                 freePares
+ * Uso:                     freePares(listaPares)
+ * ----------------------------------------------------
+ * Descripcion:             Destruye la lista, liberando recursos alocados.
+ * ----------------------------------------------------
+ *  Precondicion:           Lista que sea valida.
+ *  Postcondicion:          -
+ */
 void freePares(listaParesADT listaPares);
-void query4(listaParesADT lista, int *ok);
+
+/* Funcion:                 query4
+ * Uso:                     query4(listaPares, &ok);
+ * ----------------------------------------------------
+ * Descripcion:             Crea el archivo "pares_provincias.csv", donde cada línea de la salida contenga separados por ;
+ * 			    el par de provincias distintas
+			    (obteniendo los nombres de las mismas del archivo aeropuertos.csv) y la cantidad de
+			    movimientos de todo tipo que comparten ambas provincias. 
+ * ----------------------------------------------------
+ *  Precondicion:           Lista que sea valida y que los archivos "aeropuertos.csv", "movimientos.csv" hayan sido cargados correctamente.
+ *  Postcondicion:          -
+ */
+void query4(listaParesADT listaPares, int *ok);
 #endif
