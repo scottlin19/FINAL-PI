5db4fbd173f4c38f22e2059b4806893cb4da6e49
scottlin19 <scott.lin099@gmail.com>
2018-12-15 16:42:16 -0300

    comentadas las funciones

--- listaParesADT.h
+++ listaParesADT.h
@@ -3,7 +3,6 @@
 
 typedef struct listaParesCDT * listaParesADT;
 
-int insertarPares(listaParesADT lista,char * provincias[]);
 
 /* Funcion:                 nuevaListaPares
  * Uso:                     listaPares = nuevaListaPares();
@@ -15,10 +14,23 @@ int insertarPares(listaParesADT lista,char * provincias[]);
  */
 listaParesADT nuevaListaPares(void);
 
+/* Funcion:                 insertarPares
+ * Uso:                     insertarPares(listaPares, provincias);
+ * ----------------------------------------------------
+ * Descripcion:             Inserta un nuevo nodo con un par de provincias que comparten movimiento
+ * 			    a la lista ordenada por movimientos compartidos entre provincias.
+ * 			    Si el par ya existia, le suma un movimiento y retorna 1.
+ * 			    Retorna 1 si lo pudo insertar y 0 en caso contrario.
+ * ----------------------------------------------------
+ *  Precondicion:           Lista que sea valida.
+ *  Postcondicion:          Lista con un nuevo nodo si no estaban repetidos los pares de provincias.
+ */
+int insertarPares(listaParesADT lista,char * provincias[]);
+
 void printListaPares(listaParesADT lista);
 
 /* Funcion:                 freePares
- * Uso:                     freePares(listaPares)
+ * Uso:                     freePares(listaPares);
  * ----------------------------------------------------
  * Descripcion:             Destruye la lista, liberando recursos alocados.
  * ----------------------------------------------------
@@ -32,7 +44,7 @@ void freePares(listaParesADT listaPares);
  * ----------------------------------------------------
  * Descripcion:             Crea el archivo "pares_provincias.csv", donde cada línea de la salida contenga separados por ;
  * 			    el par de provincias distintas
-			    (obteniendo los nombres de las mismas del archivo aeropuertos.csv) y la cantidad de
+			    (obteniendo los nombres de las mismas del archivo "aeropuertos.csv") y la cantidad de
 			    movimientos de todo tipo que comparten ambas provincias. 
  * ----------------------------------------------------
  *  Precondicion:           Lista que sea valida y que los archivos "aeropuertos.csv", "movimientos.csv" hayan sido cargados correctamente.
