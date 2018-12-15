a516a2e861713c8ce2a14803181674c3d6524893
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2018-12-15 17:01:13 -0300

    Update listaAerolineaADT.h

--- listaAerolineaADT.h
+++ listaAerolineaADT.h
@@ -2,28 +2,35 @@
 #define LISTAALADT_H_
 typedef struct listaAerolineaCDT* listaAerolineaADT;
 
-/* Funcion:                nuevaListaAL
- * Uso:                    listaAL = nuevaListaAL();
- * ---------------------------------------------
- * Descripcion:            Inicializa la lista, no hay errores posibles.
- * ---------------------------------------------
- * Precondicion:           -
- * Postcondicion:          Lista creada.
+
+/* Funcion:                 nuevaListaAL
+ * Uso:                     listaAL = nuevaListaPares();
+ * ----------------------------------------------------
+ * Descripcion:             Inicializa la lista de aerolíneas. No hay error posible.
+ * ----------------------------------------------------
+ *  Precondicion:           
+ *  Postcondicion:          Lista creada.
  */
 listaAerolineaADT nuevaListaAL(void);
 
-/* Funcion:                freeAL
- * Uso:                    freeAL(listaAL);
- * ---------------------------------------------
- * Descripcion:            Destruye la lista, liberandorecursos alocados.
- * ---------------------------------------------
- * Precondicion:           Lista que sea valida. 
- * Postcondicion:          -
+
+
+/* Funcion:                 insertarAL
+ * Uso:                     insertarAL(listaAL, nombre);
+ * ----------------------------------------------------
+ * Descripcion:             Inserta un nuevo nodo con el nombre de la aerolínea, y le suma 1 movimiento.
+ * 			                    Si la aerolínea ya existia, le suma un movimiento.
+ * 			                    Retorna 1 si lo pudo insertar y 0 en caso contrario.
+ * ----------------------------------------------------
+ *  Precondicion:           Lista que sea válida, previamente creada.
+ *  Postcondicion:          Lista con un nuevo nodo si no existía dicha aerolinea ya en ella.
  */
+int insertarAL( listaAerolineaADT lista, char * nombre);
+
+int esAerolinea(char * nombre);
+
 void freeAL(listaAerolineaADT listaAL);
 
-int insertarAL( listaAerolineaADT lista, char * nombre);
-int esAerolinea(char * nombre);
 void query5(listaAerolineaADT listaAL, int *ok);
 
 #endif
