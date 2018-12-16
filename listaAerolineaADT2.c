2617d6057b3a87f25789ba9eb64f3684b569287e
scottlin19 <scott.lin099@gmail.com>
2018-12-15 22:50:23 -0300

    hola

--- listaAerolineaADT2.c
+++ listaAerolineaADT2.c
@@ -114,7 +114,9 @@ void query5(listaAerolineaADT listaAL, int *ok){
 		tAerolineaP aux = listaAL->primero;
 		int porcentaje;
 		while (aux != NULL){
+			printf("originalmente vale %g\n", ((float)aux->cant_mov_cabotaje / listaAL->cabotaje_total) * 100);
 			porcentaje = (aux->cant_mov_cabotaje / listaAL->cabotaje_total) * 100;
+			printf("despues vale %d\n", porcentaje);
 			if (porcentaje > 0){
 				fprintf(archivoDest, "%s;%d%%\n", aux->nombre, porcentaje);
 			}
